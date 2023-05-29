ee4cb1a2b4dbcfa3fe142eed6b509c86abb49f6f
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2023-05-29 13:39:28 -0400

    initial structure

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ml_raman
 New repository for the Raman spectra prediction of graphene from machine learning models
 
+
+### Installation
+
+1. Install mlcalcdriver and schnetpack first
+2. `pip install -e .`
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+requirements = ["abipy==0.9.3", "h5py==3.8.0"]
+
+setup(
+    name="ml_raman",
+    description="New repository for the Raman spectra prediction of graphene from machine learning models.",
+    packages=["ml_raman"],
+    install_requires=requirements,
+    classifiers=["Programming Language :: Python :: 3.10"],
+)
